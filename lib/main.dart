45dfd6a81992c8811747df537f74428d3b4be95c
Ayush783 <ayushc45xxx@gmail.com>
2021-11-10 15:39:59 +0530

    initialized project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Rootally());
+}
+
+class Rootally extends StatelessWidget {
+  const Rootally({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
