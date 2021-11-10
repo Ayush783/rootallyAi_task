5963e62f878083d3c52b6247d1a3222ae1b0f923
Ayush783 <ayushc45xxx@gmail.com>
2021-11-10 15:56:35 +0530

    user model implemented

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,1 +1,19 @@
-class Usermodel {}
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Usermodel {
+  final dynamic user;
+  final bool error;
+  final String errorMessage;
+
+  Usermodel(this.user, this.errorMessage, this.error);
+
+  Usermodel.success(User user)
+      : user = user,
+        error = false,
+        errorMessage = "none";
+
+  Usermodel.failure(String errorMessage)
+      : user = null,
+        error = true,
+        errorMessage = errorMessage;
+}
