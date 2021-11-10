5a443ba8073fb187bd2b36a98df2fb8b132794c7
Ayush783 <ayushc45xxx@gmail.com>
2021-11-10 16:01:13 +0530

    implemented auth service

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,1 +1,57 @@
-class AuthService {}
+//@dart=2.9
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:rootallyai_task/models/user_model.dart';
+
+class AuthService {
+  static final _auth = FirebaseAuth.instance;
+
+  //sign up
+  Future<Usermodel> signUp(
+      {@required String email, @required String password}) async {
+    try {
+      final userCred = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return Usermodel.success(userCred.user);
+    } catch (e) {
+      return Usermodel.failure(e.toString());
+    }
+  }
+
+  //sign in
+  Future<Usermodel> signIn(
+      {@required String email, @required String password}) async {
+    try {
+      final userCred = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      final isVerified = checkEmailVerification(userCred.user);
+      if (isVerified)
+        return Usermodel.success(userCred.user);
+      else
+        return Usermodel.failure('Email not verified');
+    } catch (e) {
+      return Usermodel.failure(e.toString());
+    }
+  }
+
+  // auth state changes
+  Stream<User> authStateChanges() async* {
+    yield* _auth.authStateChanges();
+  }
+
+  //vsend link to verify email
+  Future<void> verifyEmail() async {
+    await _auth.currentUser.sendEmailVerification();
+  }
+
+  bool checkEmailVerification(User user) {
+    final isVerified = user.emailVerified;
+    return isVerified;
+  }
+
+  //sign out
+  Future<void> signout() async {
+    await _auth.signOut();
+  }
+}
