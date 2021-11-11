88ffec83a43e0cf32752df895379ab712fbc27c9
Ayush783 <ayushc45xxx@gmail.com>
2021-11-11 22:21:16 +0530

    minor change

--- lib/services/database/firebase_database.dart
+++ lib/services/database/firebase_database.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:rootallyai_task/models/sessions.dart';
 
+//service for communicating with database
 class DatabaseService {
   static final _database = FirebaseDatabase.instance.reference().child('users');
   Future<SessionList> getSessions(String userId) async {
     try {
-      final snapshot = await _database.child(userId).get();
+      final snapshot = await _database
+          .child('ZvivlLcROdVhXF3XPZMT1ctLvJt2')
+          .get(); // hardcoded user id
       final data = List.from(snapshot.value).map((e) {
         final Map<String, dynamic> json = Map<String, dynamic>.from(e);
         return Sessions.fromJson(json);
